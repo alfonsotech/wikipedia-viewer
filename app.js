39ae897506ee1ab9f7d851618e5064d809c2bbc2
dratech <alfonsotech@nym.hush.com>
2016-08-12 18:39:11 -0700

    added random button for random entry get

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-var showSearchResults = function(query) {
-	var results = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + query + '&prop=revisions&rvprop=content&format=json';
-	return results;
-};
-
-var showError = function () {
-
-};
-
-var getWikied = function (tags) {
-	var query = {
-		site: 'wikipedia'
-		action: 'query'
-	}
-
-	var searchQuery = tags;
-
-	$.ajax({
-		action: "query"
-		url: "https://en.wikipedia.org/w/api.php", 
-		""
-		format: "json",
-		
-		type: "GET"
-	})
-	.done (function(){
-		var searchResults = showSearchResults();
-		$('.search-results').html(searchResults);
-	});
-	.fail (function (error) {
-		$('.search-results').append(error);
-	});
-
-}; //getWikied close
-
 $(document).ready(function(){
 
+	//Serve Up random Wiki-page on click of "random" button
+	$('#random').on('click', function(){
+		console.log("random button clicked");
+		//var randomURL =  "";
+		$.getJSON('https://en.wikipedia.org/wiki/Special:Random', function(json){
+			$('.search-results').html(JSON.stringify(json));
+			
+		});
+	});
+
+	//Global Variables
+	var searchFor;
+	//On Click, store search term in var
+		$('#submitButton').on('click', function(){
+			console.log("sub button clicked");
+			searchFor = $('#search-term').val();
+			console.log(searchFor);
+		});
+
 });//doc ready close
+
+//Prepare JSON Get
+/*var url = ''
+$.getJSON('url', function(data){
+	for (var i = 0; i<data.length; i++) {
+
+	}
+
+});
+
+---
+$(document).ready(function() {
+
+    $("#getMessage").on("click", function(){
+      // Only change code below this line.
+      
+      
+      
+      $.getJSON('/json/cats.json', function(json){
+        $('.message').html(JSON.stringify(json));
+      });
+      // Only change code above this line.
+    });
+
+  });
+
+*/
+
+//Post to search-results
