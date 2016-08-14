f87fb32891b3a6a8dd2211644eea100e07106f67
dratech <alfonsotech@nym.hush.com>
2016-08-14 15:30:36 -0700

    restoring gh-pages version

--- app.js
+++ app.js
@@ -1,38 +1,59 @@
-var showSearchResults = function(query) {
-	var results = 'https://en.wikipedia.org/w/api.php?action=query&titles=' + query + '&prop=revisions&rvprop=content&format=json';
-	return results;
-};
-
-var showError = function () {
-
-};
-
-var getWikied = function (tags) {
-	var query = {
-		site: 'wikipedia'
-		action: 'query'
-	}
-
-	var searchQuery = tags;
-
-	$.ajax({
-		action: "query"
-		url: "https://en.wikipedia.org/w/api.php", 
-		""
-		format: "json",
-		
-		type: "GET"
-	})
-	.done (function(){
-		var searchResults = showSearchResults();
-		$('.search-results').html(searchResults);
-	});
-	.fail (function (error) {
-		$('.search-results').append(error);
-	});
-
-}; //getWikied close
-
 $(document).ready(function(){
 
+	//Serve Up random Wiki-page to serach-results div on click of "random" button
+	$('#random').on('click', function(){
+		
+		//var url = 'https://en.wikipedia.org/wiki/Special:Random';
+		//window.open(url);
+		$('.search-results').append( '<iframe src="https://en.wikipedia.org/wiki/Special:Random" width="100%" height="500" frameborder="0" allowfullscreen></iframe>' );
+		
+
+	});
+
+	//Global Variables
+	var searchFor = "blank_for_test";
+	var url;
+
+	//On Click, store search term in var and url
+		$('#submitButton').on('click', function(){
+				
+			searchFor = $('#search-term').val();
+				console.log(searchFor);
+			url = 'https://en.wikipedia.org/w/api.php?action=query&format=jsonfm&list=search&utf8=1&srsearch=' + searchFor;
+				console.log(url);
+			//post();
+		});
+
+	//Get Request
+	
+
+	 //function post () {
+	 	$.getJSON(url, function(data){
+		var test= data.search[0].title;
+			//Post to search-results
+			consolelog(test);
+			$('.search-results').html(test);
+      	});
+	// }
+		
+
 });//doc ready close
+
+/*---
+$(document).ready(function() {
+
+    $("#getMessage").on("click", function(){
+      // Only change code below this line.
+      
+      
+      
+      $.getJSON('/json/cats.json', function(json){
+        $('.message').html(JSON.stringify(json));
+      });
+      // Only change code above this line.
+    });
+
+  });
+
+*/
+
